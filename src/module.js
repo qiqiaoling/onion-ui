014aa314b70242d969e23efb91837438a86ac667
qiqiaoling <352321572@qq.com>
2017-11-16 16:00:55 +0800

    test

--- src/module.js
+++ src/module.js
@@ -1,3 +1,4 @@
+import Style from './modules/style'
 
 import Button from './modules/button'
 import CheckboxGroup from './modules/checkbox-group'
@@ -5,6 +6,7 @@ import CheckboxGroup from './modules/checkbox-group'
 const Checkbox = CheckboxGroup.Checkbox
 
 export {
+  Style,
   Button,
   Checkbox,
   CheckboxGroup
